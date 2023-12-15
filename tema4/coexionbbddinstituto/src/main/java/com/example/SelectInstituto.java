1074d26ff271c36ed54fe53465d880b057d46023
BrunoRF <brunorf15@gmail.com>
2023-12-15 16:18:06 +0100

    formatted

--- tema4/coexionbbddinstituto/src/main/java/com/example/SelectInstituto.java
+++ tema4/coexionbbddinstituto/src/main/java/com/example/SelectInstituto.java
@@ -7,42 +7,39 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class SelectInstituto {
-        public static void main(String[] args) {   
-        
-        Connection con=null;
-        Statement sentencia;        
-        String url= "jdbc:mysql://localhost:3306/instituto";
-        
+    public static void main(String[] args) {
+
+        Connection con = null;
+        Statement sentencia;
+        String url = "jdbc:mysql://localhost:3306/instituto";
 
         try {
-            
+
             con = DriverManager.getConnection(url, "alumno", "0123456789");
-            
+
             String sql = "SELECT * FROM alumnos";
             sentencia = con.createStatement();
-            ResultSet rs = sentencia.executeQuery(sql); 
+            ResultSet rs = sentencia.executeQuery(sql);
 
             while (rs.next()) {
                 System.out.println("----------");
-                //Columna num
-                System.out.println("Número de alumno: "+rs.getInt("num"));
-                //Columna nombre
-                System.out.println("Nombre del alumno: "+rs.getString ("nombre"));
-                //Columna fnac
-                System.out.println("Fecha nacimiento del alumno: "+rs.getString("fnac"));
-                //Columna media
-                System.out.println("Media del alumno: "+rs.getDouble("media"));
-                //Columna curso
-                System.out.println("Curso del alumno: "+rs.getString("curso"));
+                // Columna num
+                System.out.println("Número de alumno: " + rs.getInt("num"));
+                // Columna nombre
+                System.out.println("Nombre del alumno: " + rs.getString("nombre"));
+                // Columna fnac
+                System.out.println("Fecha nacimiento del alumno: " + rs.getString("fnac"));
+                // Columna media
+                System.out.println("Media del alumno: " + rs.getDouble("media"));
+                // Columna curso
+                System.out.println("Curso del alumno: " + rs.getString("curso"));
                 System.out.println("----------");
-                System.out.println(); 
+                System.out.println();
             }
 
-        }
-        catch (SQLException  sqlex) {
+        } catch (SQLException sqlex) {
             sqlex.printStackTrace();
-        }
-        finally {
+        } finally {
             if (con != null)
                 try {
                     con.close();
