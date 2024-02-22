c5c2566fdecc025ed0f6872c8bf70d03c3649469
BrunoRF <brunorf15@gmail.com>
2024-02-22 17:52:54 +0100

    act9 added

---
+++ tema4/coexionbbddinstituto/src/main/java/com/example/actividad9/dao/EquiposDAOImplementationBruno.java
@@ -0,0 +1,96 @@
+package com.example.actividad9.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.actividad9.model.*;
+import com.example.actividad9.util.*;
+
+public class EquiposDAOImplementationBruno implements EquiposDAOBruno {
+
+    @Override
+    public void insertarEquipo(Equipo equipo) {
+        try (Connection connection = DatasourceConnectionBruno.getConnection()) {
+            String sql = "INSERT INTO equipos (nombre, ciudad, conferencia, division) VALUES (?, ?, ?, ?)";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, equipo.getNombreEquipo());
+            statement.setString(2, equipo.getCiudad());
+            statement.setString(3, equipo.getConferencia());
+            statement.setString(4, equipo.getDivision());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void actualizarEquipo(Equipo equipo) {
+        try (Connection connection = DatasourceConnectionBruno.getConnection()) {
+            String sql = "UPDATE equipos SET ciudad = ?, conferencia = ?, division = ? WHERE nombre = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, equipo.getCiudad());
+            statement.setString(2, equipo.getConferencia());
+            statement.setString(3, equipo.getDivision());
+            statement.setString(4, equipo.getNombreEquipo());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void eliminarEquipo(String nombre) {
+        try (Connection connection = DatasourceConnectionBruno.getConnection()) {
+            String sql = "DELETE FROM equipos WHERE nombre = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, nombre);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Equipo obtenerEquipo(String nombre) {
+        Equipo equipo = null;
+        try (Connection connection = DatasourceConnectionBruno.getConnection()) {
+            String sql = "SELECT * FROM equipos WHERE nombre = ?";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, nombre);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                equipo = new Equipo(resultSet.getString("nombre"),
+                                            resultSet.getString("ciudad"),
+                                            resultSet.getString("conferencia"),
+                                            resultSet.getString("division"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return equipo;
+    }
+
+    @Override
+    public List<Equipo> obtenerTodosEquipos() {
+        List<Equipo> equipos = new ArrayList<>();
+        try (Connection connection = DatasourceConnectionBruno.getConnection()) {
+            String sql = "SELECT * FROM equipos";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Equipo equipo = new Equipo(resultSet.getString("nombre"),
+                                            resultSet.getString("ciudad"),
+                                            resultSet.getString("conferencia"),
+                                            resultSet.getString("division"));
+                equipos.add(equipo);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return equipos;
+    }
+}
---
+++ tema4/coexionbbddinstituto/src/main/java/com/example/actividad9/util/DatasourceConnectionBruno.java
@@ -0,0 +1,28 @@
+package com.example.actividad9.util;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class DatasourceConnectionBruno {
+
+    private static DataSource dataSource;
+
+    static {
+        try {
+            Context initContext = new InitialContext();
+            Context envContext = (Context) initContext.lookup("java:/comp/env");
+            dataSource = (DataSource) envContext.lookup("jdbc/nba");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+}
